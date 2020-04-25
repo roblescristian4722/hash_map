66a08485ec496c1e3891d2922d1217b67dc1c4c0
kristoferrobin66 <roblescristian991@gmail.com>
2020-04-24 20:07:07 -0500

    Eliminando funciones extra y cambiando lsl de buckets por un array

--- hash_map.h
+++ hash_map.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <sstream>
 #include "lsl.h"
+#define PRIMO 211
 #define DEFAULT_MOD 13
 using namespace std;
 
@@ -15,14 +16,6 @@ template <typename K, typename V>
 class Bucket;
 template <typename K, typename V>
 class HashMap;
-
-/////////////////////////
-//// EXTRA FUNCTIONS ////
-/////////////////////////
-size_t string_to_size_t(string str);
-
-template <typename T>
-long binary_search(LSL<T> &list, T data);
 
 //////////////
 //// NODE ////
@@ -91,13 +84,12 @@ template <typename K, typename V>
 class HashMap
 {
 private:
-    LSL<Bucket<K, V>> m_buckets;
-    size_t m_mod;
+    Bucket<K, V>* m_buckets;
+    size_t m_size;
 
 public:
-    HashMap(size_t mod = DEFAULT_MOD) : m_mod(mod) {}
-    ~HashMap()
-    { clear(); }
+    HashMap();
+    ~HashMap();
 
     struct Pair
     {
@@ -105,8 +97,8 @@ public:
         V *value;
     };
 
-    bool is_empty();
-    long is_colide(size_t hash);
+    bool empty();
+    long colide(size_t hash);
     size_t size();
 
     void insert(const K &keyVal, const V &val);
@@ -122,19 +114,30 @@ public:
 //// DEFINITIONS  ////
 //////////////////////
 
+template <typename K, typename V>
+HashMap<K, V>::HashMap()
+{
+    m_size = 0;
+    m_buckets = new Bucket<K,V>[PRIMO];
+}
+
+template<typename K, typename V>
+HashMap<K, V>::~HashMap()
+{ clear(); }
+
 // Returns true if empty
 // and false otherwise
 template <typename K, typename V>
-bool HashMap<K, V>::is_empty()
-{ return m_buckets.empty(); }
+bool HashMap<K, V>::empty()
+{ return m_size }
 
 // indicates wheter the hash value returned
 // by hash_function already exists and if it does this
 // method gives you it's exact position
 template <typename K, typename V>
-long HashMap<K, V>::is_colide(size_t hash)
+long HashMap<K, V>::colide(size_t hash)
 {
-    if (is_empty())
+    if (empty())
         return -1;
 
     Bucket<K, V> bucketTmp;
@@ -157,22 +160,22 @@ size_t HashMap<K, V>::size()
 template <typename K, typename V>
 void HashMap<K, V>::insert(const K &keyVal, const V &val)
 {
-    Node<K, V> n(keyVal, val);
-    Bucket<K, V> b;
+    Node<K, V> nodeTmp(keyVal, val);
+    Bucket<K, V> bucketTmp;
     long pos;
     size_t hash;
 
     hash = hash_function(keyVal);
-    pos = is_colide(hash);
+    pos = colide(hash);
     if (pos != -1) {
-        m_buckets[pos].append(n);
+        m_buckets[pos].append(nodeTmp);
         sort(m_buckets[pos].m_nodes);
     }
 
     else {
-        b.m_hashValue = hash;
-        b.append(n);
-        m_buckets.push_back(b);
+        bucketTmp.m_hashValue = hash;
+        bucketTmp.append(nodeTmp);
+        m_buckets.push_back(bucketTmp);
         sort(m_buckets);
     }
 }
@@ -184,7 +187,7 @@ void HashMap<K, V>::delete_value(const K &keyVal)
     size_t aux = hash_function(keyVal);
     size_t i;
     LSL<Node<K, V>> *auxList;
-    long pos = is_colide(hash_function(keyVal));
+    long pos = colide(hash_function(keyVal));
 
     if (pos != -1){
         auxList = &m_buckets[pos].m_nodes;
@@ -208,13 +211,30 @@ template <typename K, typename V>
 void HashMap<K, V>::clear()
 { m_buckets.clear(); }
 
+// It gets the key's corresponding hash
+// value using the PJW method
 template <typename K, typename V>
 size_t HashMap<K, V>::hash_function(K val)
 {
+    // The input turns into a stringstream value
+    // and then turns into a string due to
+    // possible numeric keys
     stringstream ss;
+    char* pos;
+    size_t hash = 0;
+    size_t tmp;
+    
     ss << val;
-    string aux = ss.str();
-    return string_to_size_t(aux) % m_mod;
+    string auxStr = ss.str();
+    for (pos = auxStr; *pos != '\0'; ++pos){
+        hash = (hash << 4) + (*pos);
+        tmp = hash & 0xF0000000;
+        if (tmp > 1){
+            hash = hash ^ (tmp << 24);
+            hash = hash ^tmp;
+        }
+    }
+    return hash;
 }
 
 // Returns a value stored in the hash map
@@ -226,7 +246,7 @@ V *HashMap<K, V>::operator[](K index)
     Node<K, V> nodeTmp;
     long posNode;
     size_t hashValue = hash_function(index);
-    long posBucket = is_colide(hashValue);
+    long posBucket = colide(hashValue);
     V *v = nullptr;
 
     nodeTmp.key = index;
@@ -263,35 +283,6 @@ typename HashMap<K,V>::Pair HashMap<K, V>::get_position(size_t index)
         }
     }
     return pair;
-}
-
-/////////////////////////
-//// EXTRA FUNCTIONS ////
-//// DEFINITIONS     ////
-/////////////////////////
-size_t string_to_size_t(string str)
-{
-    size_t sum = 0;
-    for (size_t i = 0; i < str.length(); ++i)
-        sum += (size_t)str[i];
-    return sum + (size_t)str.length();
-}
-
-template <typename T>
-long binary_search(LSL<T> &list, T data)
-{
-    int l = 0;
-    int r = int(list.size() - 1);
-    while (l <= r) {
-        int m = (l + r) / 2;
-        if (data == list[m])
-            return m;
-        else if (data < list[m])
-            r = m - 1;
-        else
-            l = m + 1;
-    }
-    return -1;
 }
 
 #endif //HASHMAP_H
